e417a44c62f91d20916577dc63bae3328b2e8fa3
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-01-13 13:13:46 -0500

    sample api receiving

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,13 +1,14 @@
 import "../styles/style.css";
-function greet(name) {
-  const greetPromise = new Promise(function (resolve, reject) {
-    resolve(`Hello ${name}`);
-  });
-  return greetPromise;
-}
 
-const suzie = greet("Suzie");
-console.log(suzie);
-suzie.then((result) => {
-  console.log(result);
-});
+const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("app").textContent = data.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
