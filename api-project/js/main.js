5eeda5316bbddac4ea97654219e80e8b5e2d9743
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-01-18 13:13:54 -0500

    wow i did almost nothing

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,12 +1,12 @@
 import "../styles/style.css";
 import { DOM } from "./dom";
 function changeQuote() {
-  const URL = "https://api.quotable.io/random";
+  const URL = "https://swapi.dev/api/people/1/";
   async function getData(URL) {
     try {
       const response = await fetch(URL);
       const data = await response.json();
-      DOM.quote.textContent = data.content;
+      DOM.quote.textContent = data.homeworld;
     } catch (error) {
       console.log(error);
     }
