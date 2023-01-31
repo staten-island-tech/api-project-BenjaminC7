16635d79fa95a06390cf74ff87f50fc43875ecad
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-01-31 12:46:42 -0500

    laptopWork

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,23 +2,23 @@ import "../styles/style.css";
 import { DOM } from "./dom";
 
 function createClassButtons() {
-  const URL = `https://www.dnd5eapi.co/api/classes`;
+  const link = `https://www.dnd5eapi.co/api/classes`;
   async function getData(URL) {
     try {
       const response = await fetch(URL);
       const data = await response.json();
-      data.forEach((role) => {
-        DOM.classBtns.insertAdjacentHTML(
-          "afterbegin",
-          `<button id=${role.name}Btn" class="btn">${role.name}</button>`
-        );
-        console.log(data.name);
-      });
+      return data;
     } catch (error) {
       console.log(error);
     }
+    getData(link);
+    data.forEach((role) => {
+      DOM.classBtns.insertAdjacentHTML(
+        "afterbegin",
+        `<button id=${role.name}Btn" class="btn">${role.name}</button>`
+      );
+    });
   }
-  getData(URL);
 }
 createClassButtons();
 function insertClassDescription(role) {
